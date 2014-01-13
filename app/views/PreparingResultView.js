33a7883f9f012952fe1fda171f2f2270e58e6eeb
gracian.ani@gmail.com <gracian.ani@gmail.com>
2014-01-13 19:16:15 +0800

    with answer

---
+++ app/views/PreparingResultView.js
@@ -0,0 +1,11 @@
+﻿define(["jquery", "backbone", "mustache", "text!templates/InQuiz.html", "animationscheduler", "views/QuestionView"],
+
+    function ($, Backbone, Mustache, template, AnimationScheduler, QuestionView) {
+
+        var InQuizView = Backbone.View.extend({
+
+        });
+
+        return InQuizView;
+     }
+}
